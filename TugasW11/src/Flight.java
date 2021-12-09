e4f65403471fb82a228043979e2a832fa4c17c61
yoganizar7 <yoga.nizar.tif20@polban.ac.id>
2021-12-10 03:15:49 +0700

    pengumpulan tugas w11

---
+++ TugasW11/src/Flight.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.*;
+
+public abstract class Flight {
+	
+	 private String id;
+	 List<Passenger> passengers = new ArrayList<Passenger>();
+	 
+	 public Flight(String id) {
+		 this.id = id;
+	 }
+	 
+	 public String getId() {
+		 return id;
+	 }
+	 
+	 public List<Passenger> getPassengersList() {
+		 return Collections.unmodifiableList(passengers);
+	 }
+	 
+	 public abstract boolean addPassenger(Passenger passenger);
+	 public abstract boolean removePassenger(Passenger passenger);
+}
---
+++ TugasW11/src/economyFlight.java
@@ -0,0 +1,18 @@
+
+public class economyFlight extends Flight {
+	public economyFlight(String id) {
+		 super(id);
+		 }
+	
+	@Override
+	 public boolean addPassenger(Passenger passenger) {
+	 return passengers.add(passenger);
+	 }
+	 @Override
+	 public boolean removePassenger(Passenger passenger) {
+	 if (!passenger.isVip()) {
+	 return passengers.remove(passenger);
+	 }
+	 return false;
+	 }
+}
